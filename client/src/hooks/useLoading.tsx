d274da1d48d25c8318e7c97e74dd95240aa0e719
Marcos Andrade <marcospandrade2@gmail.com>
2024-07-11 23:13:10 -0300

    feat: create loading component and custom hook

---
+++ client/src/hooks/useLoading.tsx
@@ -0,0 +1,48 @@
+'use client'
+
+import { createContext, ReactNode, useContext, useMemo, useState } from "react";
+import { Loading } from "@/components/common/Loading";
+
+interface LoadingContextData {
+    statusLoading: boolean;
+    changeLoadingStatus: (status: boolean) => void;
+}
+
+interface LoadingContextProviderProps {
+    children: ReactNode;
+}
+
+const LoadingContext = createContext<LoadingContextData>(
+    {} as LoadingContextData
+);
+
+function LoadingContextProvider({ children }: LoadingContextProviderProps) {
+    const [statusLoading, setStatusLoading] = useState<boolean>(false);
+
+    function changeLoadingStatus(status: boolean) {
+        setStatusLoading(status);
+    }
+
+    const loadingContextData: LoadingContextData = useMemo(
+        () => ({
+            statusLoading,
+            changeLoadingStatus
+        }),
+        [statusLoading, changeLoadingStatus]
+    );
+
+    return (
+        <LoadingContext.Provider value={loadingContextData}>
+            {/* <ReactLoading type="spin" height="5%" width="5%" color="#000000" /> */}
+            {statusLoading && (<Loading />)}
+            {children}
+        </LoadingContext.Provider>
+    );
+}
+
+function useLoading() {
+    const context = useContext(LoadingContext);
+    return context;
+}
+
+export { LoadingContextProvider, useLoading };
