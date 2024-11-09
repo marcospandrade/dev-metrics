1febf2e81a3bdfb4d6a1eed84694c49a6a8f7c31
Marcos Andrade <marcospandrade2@gmail.com>
2024-11-09 17:17:03 -0300

    fix: add project id as conflict path for upsert Custom Field

--- server/src/modules/integration-server/commands/register-custom-fields/register-custom-fields.handler.ts
+++ server/src/modules/integration-server/commands/register-custom-fields/register-custom-fields.handler.ts
@@ -4,7 +4,6 @@ import { RegisterCustomFieldsCommand } from './register-custom-fields.command';
 import { SchemaValidator } from '@core/utils';
 import { CustomFieldsUseCases } from '@modules/integration-server/use-cases/custom-fields.use-cases.service';
 import { UpsertCustomFieldDto } from '@modules/integration-server/dto/upsert-custom-field.dto';
-import { IntegrationServerUseCases } from '@modules/integration-server/use-cases/integration-server.use-cases.service';
 import { CheckSyncProjectEvent } from '@modules/integration-server/events/check-sync-project.event';
 import { ProjectUseCases } from '@modules/integration-server/use-cases/projects.use-cases.service';
 
@@ -14,12 +13,9 @@ export class RegisterCustomFieldsHandler implements ICommandHandler<RegisterCust
         private readonly eventBus: EventBus,
         private readonly customFieldUseCases: CustomFieldsUseCases,
         private readonly projectsUseCases: ProjectUseCases,
-        private readonly service: IntegrationServerUseCases,
     ) {}
 
     async execute(command: RegisterCustomFieldsCommand): Promise<void> {
-        const integrationServer = await this.service.getServerByProjectId(command.projectId);
-
         const fieldsToRegister: UpsertCustomFieldDto[] = command.fieldsToRegister.map(field => {
             return SchemaValidator.toInstance(
                 {
@@ -33,10 +29,12 @@ export class RegisterCustomFieldsHandler implements ICommandHandler<RegisterCust
             );
         });
 
-        await this.customFieldUseCases.upsertCustomField(fieldsToRegister);
-        await this.projectsUseCases.updateOne(command.projectId, {
-            isCustomFieldSelected: true,
-        });
+        await Promise.all([
+            await this.customFieldUseCases.upsertCustomField(fieldsToRegister),
+            await this.projectsUseCases.updateOne(command.projectId, {
+                isCustomFieldSelected: true,
+            }),
+        ]);
 
         return this.eventBus.publish<CheckSyncProjectEvent, void>(
             SchemaValidator.toInstance(
