7fb0c62e30bcf7596f358d56d2cafb1b9d453278
Marcos Andrade <marcospandrade2@gmail.com>
2024-11-13 11:55:27 -0300

    feat: add ignorecase as false on github

---
+++ client/src/components/Common/Loading.tsx
@@ -0,0 +1,11 @@
+import { Spinner } from '@material-tailwind/react'
+
+export function Loading() {
+  return (
+    <div className="absolute bg-gray-200 bg-opacity-60 z-[100] h-full w-full flex items-center justify-center">
+      <div className="flex items-center">
+        <Spinner type="grow" className="h-16 w-16" color="indigo" />
+      </div>
+    </div>
+  )
+}
---
+++ client/src/components/Common/SearchInput.tsx
@@ -0,0 +1,21 @@
+import { LibIcons } from "@/lib/icons";
+
+interface SearchInputProps {
+    searchInputPlaceholder: string;
+    onChangeSearchInput: (searchInput: string) => void;
+}
+
+export function SearchInput({ searchInputPlaceholder, onChangeSearchInput }: Readonly<SearchInputProps>) {
+  return (
+    <div className="relative">
+      <input
+        className="bg-white w-full pr-11 h-10 pl-3 py-2 bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded transition duration-200 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md"
+        placeholder={searchInputPlaceholder ?? 'Search here...'}
+        onChange={(e) => onChangeSearchInput(e.target.value)}
+      />
+      <button className="absolute h-8 w-8 right-1 top-1 my-auto px-2 flex items-center bg-white rounded " type="button">
+        <LibIcons.SearchIcon />
+      </button>
+    </div>
+  )
+}
