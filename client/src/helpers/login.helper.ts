2d456ce783d5b0c13d76ede50b154cd2b029163f
Marcos Andrade <marcospandrade2@gmail.com>
2024-11-18 17:02:55 -0300

    fix: add static function to generate escaped string for oauth2 callback

--- client/src/helpers/login.helper.ts
+++ client/src/helpers/login.helper.ts
@@ -1,12 +1,18 @@
-import { escape } from 'querystring'
 export class LoginHelper {
   static getLoginUrl() {
     const loginUrl = `https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=${
       process.env.NEXT_PUBLIC_JIRA_CLIENT_ID
     }&scope=${
       process.env.NEXT_PUBLIC_JIRA_SCOPES_REQUESTED
-    }&redirect_uri=${escape(process.env.NEXT_PUBLIC_LOCAL_URL!)}api%2Fauth%2Fcallback&state=${crypto.randomUUID()}&response_type=code&prompt=consent`;
+    }&redirect_uri=${this.escapeUrl(process.env.NEXT_PUBLIC_LOCAL_URL!)}api%2Fauth%2Fcallback&state=${crypto.randomUUID()}&response_type=code&prompt=consent`;
 
     return loginUrl;
+  }
+
+  static escapeUrl(url?:string) {
+    if(!url) return '';
+    return encodeURIComponent(url)
+      .replace(/[!'()*]/g, char => `%${char.charCodeAt(0).toString(16).toUpperCase()}`)
+      .replace(/%20/g, '+');
   }
 }
